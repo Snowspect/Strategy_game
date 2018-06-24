0d87c8fba6be57a2932563447778c6921f41c241
Snowspect <s155384@student.dtu.dk>
2018-06-24 22:32:32 +0200

    New Game concept
    Added first DTO to project

---
+++ Strategy_game/Data/Participant_DTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_game.Data
+{
+    class Participant_DTO
+    {
+
+        private int Health;
+        private int Defence;
+        private int Offence;
+        private String name;
+        private int VMove;
+        private int HMove;
+
+        public int VMove1 { get => VMove; set => VMove = value; }
+        public string Name { get => name; set => name = value; }
+        public int Offence1 { get => Offence; set => Offence = value; }
+        public int Defence1 { get => Defence; set => Defence = value; }
+        public int HMove1 { get => HMove; set => HMove = value; }
+        public int Health1 { get => Health; set => Health = value; }
+    }
+}
+
+//Ctrl + R + E (creates a get and set for a private variable
