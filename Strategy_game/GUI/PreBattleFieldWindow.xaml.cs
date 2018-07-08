6c1b76b72befd1739ab5a78d6472d81252fd8376
Snowspect <s155384@student.dtu.dk>
2018-07-08 09:00:22 +0200

    Prim: setting up navigation on window
    - Added navigation buttons
    - Added exit event

--- Strategy_game/GUI/PreBattleFieldWindow.xaml.cs
+++ Strategy_game/GUI/PreBattleFieldWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Xml;
-using System.Xml.Linq;
 
 namespace Strategy_game.GUI
 {
@@ -22,31 +12,63 @@ namespace Strategy_game.GUI
     /// </summary>
     public partial class PreBattleFieldWindow : Window
     {
+        MainWindow mw;
+        Window w;
+        private Boolean exitApp;
+
         public PreBattleFieldWindow()
         {
-            string XmlDocu = new Uri(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\GUI\\" + "PreBattleFieldWindow.xaml").ToString();
-            XDocument xdoc = XDocument.Load(XmlDocu);
-            Console.WriteLine(xdoc);
-            var item = xdoc.Element("FieldSizeBattle");
-            int j = 6;
-            int i = FieldSizeBattle.Columns = 3;
-            int h = 0;
-            //int size = 6 * 3;
+            InitializeComponent();
+        }
+        public PreBattleFieldWindow(MainWindow mw, Window w)
+        {
+            this.w = w;
+            this.mw = mw;
+            Closed += new EventHandler(App_exit); //subscribing to closed event
+            exitApp = true; //used for closing app
+        }
 
+        //Triggers when window is closed.
+        void App_exit(object sender, EventArgs e) /*App_exit is my own defined method.*/ { if (exitApp == true) { w.Close(); } /*closes mainWindow*/ }
+
+        // Accesses the previous window
+        private void ToPreviousWindow_Click(object sender, RoutedEventArgs e)
+        { /*do not close mw.*/ exitApp = false; /*loads mainWindow*/ if (w is MainWindow) { this.w.Show(); this.Close(); } /*loads any other window */ else { w = new Window(); w.Show(); this.Close(); } }
+
+        //Loads mainwindow
+        private void ToMenuWindow_Click(object sender, RoutedEventArgs e) { mw.Show(); exitApp = false; this.Close(); }
+
+        public void CreatePreField()
+        {
+            UserControl u;
+            int j = 6;
+            int i = 3;
+            int h = 0;
+            PreFieldBattle.Rows = 6;
+            PreFieldBattle.Columns = 3;
+            h = j;
             for (int k = 1; k < j + 1; k++) //k is 1, increased to 6
             {
-                h = j;
                 for (int g = 1; g < i + 1; g++) // g is 1, increased to 3 
                 {
-                    string xName = "x" + g + "y" + h--; //g goes 1,2,3...  
-                    item.Add("<Border BorderThickness=\"1\" BorderBrush=\"black\"><Image x:Name=" + xName + "></Image></Border>");
+                    u = new UserControl();
+                    Border b = new Border();
+                    b.BorderBrush = new SolidColorBrush(Colors.Black);
+                    b.BorderThickness = new Thickness(1);
+                    string xName = "x" + g + "y" + h; //g goes 1,2,3...  //h goes 6,5,4 repeatedly. (should go, 6,6,6 and then 5,5,5)
+
+                    Image img = new Image();
+                    img.Stretch = Stretch.Fill;
+                    string image = "SlimeBlack.png";
+                    img.Name = xName;
+                    img.Source = new BitmapImage(new Uri(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\Sources\\" + image));
+                    b.Child = img;
+
+                    u.Content = b;
+                    PreFieldBattle.Children.Add(u);
                 }
+                h--;
             }
-
-
-//            item.Add("<Image x:Name=" + xName + "></Image>");
-             
-            InitializeComponent();
         }
     }
 }
